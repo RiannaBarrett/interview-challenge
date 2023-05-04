2908ef4bde78119aca2a7ec0ab5d0f9715347f75
alexander-louderback <alex@alouderback.com>
2023-05-04 11:41:28 -0400

    initial challenge questions

---
+++ index.js
@@ -0,0 +1,17 @@
+/*  Challenge 2: JavaScript DOM Manipulation (20 minutes)
+    Task: Add interactivity to the webpage created in Challenge 1. Implement a search bar that filters the list of items based on the user's input.
+    Requirements:
+        TODO: Create a search bar using HTML and CSS. (will be in index.html and index.css, make sure to link files!)
+        TODO: Use JavaScript to listen for the 'input' event on the search bar.
+        TODO: Filter the list of items based on the user's input, hiding the items that don't match the search query.
+        TODO: Display a message if no items match the search query. */
+
+
+
+/* Challenge 3: Fetching and Rendering Data using JavaScript (25 minutes)
+    Task: Modify the webpage from Challenge 1 & 2 to fetch data about your favorite books/movies from a provided API endpoint and render the list dynamically.
+    Requirements:
+        TODO: Use the Fetch API to retrieve data from https://alouderback.github.io/Data/db.json
+        TODO: Process the received JSON data and create an HTML structure dynamically using JavaScript.
+        TODO: Render the list of items on the page, replacing the static content from Challenge 1.
+        TODO: Ensure that the search functionality from Challenge 2 still works with the dynamically generated content. */
